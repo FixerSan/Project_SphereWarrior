ce33dda4ed9f7cbfdaade2c40c7c675a501ed920
FixerSan <ggomarain1302@gmail.com>
2024-05-14 04:41:49 +0900

    [Feat] FaceController 구현

---
+++ Project_SphereWarrior/Assets/001.Scripts/Face/Face.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+    public FaceData data;
+
+    public Face(int _level)
+    {
+        data = new FaceData(_level);
+    }
+
+    public void CollisionFace()
+    {
+        Managers.Game.GetGold(data.addGoldValue);
+    }
+}
+
+public class FaceData
+{
+    public FaceData(int _level)
+    {
+        level = _level;
+        addGoldValue = _level * 1.5f;
+    }
+
+    public int level;
+    public float addGoldValue;
+}
---
+++ Project_SphereWarrior/Assets/001.Scripts/FaceController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceController : MonoBehaviour
+{
+    public Define.FaceType faceType;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("¡¢√À");
+            Managers.Game.CollisionFace(faceType);
+        }
+    }
+}
