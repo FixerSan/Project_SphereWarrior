bc51bebfca3c8bf51e658bc498896a30374ef4a3
FixerSan <ggomarain1302@gmail.com>
2024-05-16 06:22:27 +0900

    [Feat] PlayerController 충돌 반사 로직 변경

--- Project_SphereWarrior/Assets/001.Scripts/PlayerController.cs
+++ Project_SphereWarrior/Assets/001.Scripts/PlayerController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
+    public float speed; 
 
     private void Awake()
     {
-        rb.AddForce(new Vector3(-3, 4f, 5), ForceMode.Impulse);   
+        rb.AddForce(new Vector3(-3, 4f, 6), ForceMode.Impulse);   
     }
 
-    private void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(rb.velocity);
+        Vector3 collisionNormal = collision.contacts[0].normal;
+        rb.velocity = CalculateReflectDirection(collisionNormal) * speed;
+    }
+
+    private Vector3 CalculateReflectDirection(Vector3 _collisionNormalVecter)
+    {
+        Vector3 nowDir = rb.velocity.normalized;
+        Vector3 reflectDir = Vector3.Reflect(nowDir, _collisionNormalVecter).normalized;
+        return reflectDir;
     }
 }
